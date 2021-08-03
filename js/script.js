ac8040a11116ce538ef4b89f69b831191152c393
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-02 23:22:23 -0300

    Mobile Menu functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 const menuIcon = document.querySelector
 ('.hamburguer-menu');
 const navbarMobile = document.querySelector('.navbar-mobile');
-const hideMobile = document.querySelector('#hideMobile');
 
 menuIcon.addEventListener('click', () => {
     navbarMobile.classList.toggle('change');
-    hideMobile.style.display = 'none';
-    menuIcon.addEventListener('click', () =>{
-        hideMobile.style.display = 'block';
-    })
 })
