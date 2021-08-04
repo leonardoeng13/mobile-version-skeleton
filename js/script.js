20542364a2e9bc6913b6d6af82aad8e8fe35b427
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-04 16:06:22 -0300

    Linters error fix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 const menuIcon = document.querySelector('.hamburguer-menu');
 const navbarMobile = document.querySelector('.navbar-mobile');
-
 // #################### DESKTOP ##################################
-
 const modal2 = document.querySelector('#desktopSimpleModal');
 const seeProject = document.querySelectorAll('.seeproject');
 const deskCloseBtn = document.querySelector('.deskCloseBtn');
 
 //********************* NEW PART *********************************/
 
-const openPopupButtons = document.querySelectorAll('.seeproject'); //Modal Button
+const openPopupButtons = document.querySelectorAll('.seeproject');
 
 
 //********************* END PART *********************************/
@@ -19,9 +17,7 @@ const openPopupButtons = document.querySelectorAll('.seeproject'); //Modal Butto
 const modal = document.querySelector('#simpleModal');
 const btnseethis = document.querySelector('.btnseethis');
 const closeBtn = document.querySelector('.closeBtn');
-
 // ################# CLOSE POP UP MENU ###########################
-
 const menuItem1 = document.querySelector('#menu-item1');
 const menuItem2 = document.querySelector('#menu-item2');
 const menuItem3 = document.querySelector('#menu-item3');
@@ -77,51 +73,51 @@ const divProject = document.createElement('div');
 function addEvents() {
     for (let i = 0; i < openPopupButtons.length; i += 1) {
       openPopupButtons[i].addEventListener('click', (ev) => {
-        const projectId = ev.target.id;
-        divProject.classList.add('popup-overlay-objects');
-        document.querySelector('.popup-bg').style.display = 'flex';
-        let techsHTML = '';
-        for (let k = 0; k < projects[i].technologies.length; k += 1) {
-          techsHTML += `<li><p>${projects[i].technologies[k]}</p></li>`;
-        }
-        divProject.innerHTML = '<div class="close-btn">+</div>'
-        + `<div class="modal-content">`
-        + `<div class="modal-content-white">`
-        + `<h1 class="modal-title">${projects[projectId].title}</h1>`
-        + `<ul class="modal-language">${techsHTML}</ul>`
-        + '<div class="content d-flex flex-dir-col">'
-        + '<div class="content-image">'
-        + `<img src="${projects[projectId].image}" alt="modal-image" />`
-        + '</div>'
-        + '<div class="body d-flex flex-dir-col">'
-        + '<div class="paragraph-modal">'
-        + `<p>${projects[projectId].description}</p>`
-        + '<div class="d-flex pop-buttons">'
-        + '<button class="btn-primary btn-pop d-flex" type="button">'
-        + 'See Live <i class="fa fa-clock-o"></i></button>'
-        + '<div class="d-flex pop-buttons">'
-        + '<button class="btn-secondary btn-pop d-flex" type="button">'
-        + 'See Source <i class="fa fa-github"></i></button>'
-        + '</div>'
-        + '</div>'
-        + '</div>'
-        + '</div>'
-        + '</div>'
-        + '</div>'
-        + '</div>';
+      const projectId = ev.target.id;
+      divProject.classList.add('popup-overlay-objects');
+      document.querySelector('.popup-bg').style.display = 'flex';
+      let techsHTML = '';
+      for (let k = 0; k < projects[i].technologies.length; k += 1) {
+        techsHTML += `<li><p>${projects[i].technologies[k]}</p></li>`;
+      }
+      divProject.innerHTML = '<div class="close-btn">+</div>'
+      + `<div class="modal-content">`
+      + `<div class="modal-content-white">`
+      + `<h1 class="modal-title">${projects[projectId].title}</h1>`
+      + `<ul class="modal-language">${techsHTML}</ul>`
+      + `<div class="content d-flex flex-dir-col">`
+      + `<div class="content-image">`
+      + `<img src="${projects[projectId].image}" alt="modal-image" />`
+      + '</div>'
+      + '<div class="body d-flex flex-dir-col">'
+      + '<div class="paragraph-modal">'
+      + `<p>${projects[projectId].description}</p>`
+      + '<div class="d-flex pop-buttons">'
+      + '<button class="btn-primary btn-pop d-flex" type="button">'
+      + 'See Live <i class="fa fa-clock-o"></i></button>'
+      + '<div class="d-flex pop-buttons">'
+      + '<button class="btn-secondary btn-pop d-flex" type="button">'
+      + 'See Source <i class="fa fa-github"></i></button>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>';
   
-        document.getElementById('overlay').appendChild(divProject);
+    document.getElementById('overlay').appendChild(divProject);
   
-        const deleteDetail = document.querySelector('.close-btn');
-        deleteDetail.addEventListener('click', () => {
-          divProject.remove();
-          document.querySelector('.popup-bg').style.display = 'none';
-        });
+    const deleteDetail = document.querySelector('.close-btn');
+    deleteDetail.addEventListener('click', () => {
+      divProject.remove();
+      document.querySelector('.popup-bg').style.display = 'none';
       });
+    });
     }
-  }
-  addEvents();
-//********************* END PART *********************************/
+}
+
+addEvents();
 
 seeProject.addEventListener('click', () => {
   modal2.style.display = 'block';
