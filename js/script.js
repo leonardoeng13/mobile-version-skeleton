0dd1ccfdc219ccbabff791475c0296508ed0fe6c
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-04 16:16:03 -0300

    Linters error fix

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ const modal2 = document.querySelector('#desktopSimpleModal');
 const seeProject = document.querySelectorAll('.seeproject');
 const deskCloseBtn = document.querySelector('.deskCloseBtn');
 
-//********************* NEW PART *********************************/
+// ******************** NEW PART **********************************
 
 const openPopupButtons = document.querySelectorAll('.seeproject');
 
 
-//********************* END PART *********************************/
+// ********************* END PART ********************************
 
 // #################### MOBILE ###################################
 
@@ -43,7 +43,7 @@ menuItem4.addEventListener('click', () => {
   navbarMobile.classList.toggle('change');
 });
 
-//********************* NEW PART *********************************/
+// ********************* NEW PART ********************************
 
 const projects = [
   {
@@ -52,7 +52,7 @@ const projects = [
     image: 'images/Group-105.svg',
     technologies: ['HTML/CSS', 'Ruby and Rails', 'JavaScript'],
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     liveLink: '#',
     sourceLink: '#',
   },
@@ -62,17 +62,17 @@ const projects = [
     image: 'images/Group-105.svg',
     technologies: ['HTML/CSS', 'Ruby and Rails', '+1'],
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     liveLink: '#',
     sourceLink: '#',
   },
 ];
-//********************* END PART *********************************/
+// ********************* END PART *********************************
 const divProject = document.createElement('div');
 
 function addEvents() {
-    for (let i = 0; i < openPopupButtons.length; i += 1) {
-      openPopupButtons[i].addEventListener('click', (ev) => {
+  for (let i = 0; i < openPopupButtons.length; i += 1) {
+    openPopupButtons[i].addEventListener('click', (ev) => {
       const projectId = ev.target.id;
       divProject.classList.add('popup-overlay-objects');
       document.querySelector('.popup-bg').style.display = 'flex';
@@ -88,23 +88,23 @@ function addEvents() {
       + `<div class="content d-flex flex-dir-col">`
       + `<div class="content-image">`
       + `<img src="${projects[projectId].image}" alt="modal-image" />`
-      + '</div>'
-      + '<div class="body d-flex flex-dir-col">'
-      + '<div class="paragraph-modal">'
+      + `</div>`
+      + `<div class="body d-flex flex-dir-col">`
+      + `<div class="paragraph-modal">`
       + `<p>${projects[projectId].description}</p>`
-      + '<div class="d-flex pop-buttons">'
-      + '<button class="btn-primary btn-pop d-flex" type="button">'
-      + 'See Live <i class="fa fa-clock-o"></i></button>'
-      + '<div class="d-flex pop-buttons">'
-      + '<button class="btn-secondary btn-pop d-flex" type="button">'
-      + 'See Source <i class="fa fa-github"></i></button>'
-      + '</div>'
-      + '</div>'
-      + '</div>'
-      + '</div>'
-      + '</div>'
-      + '</div>'
-      + '</div>';
+      + `<div class="d-flex pop-buttons">`
+      + `<button class="btn-primary btn-pop d-flex" type="button">`
+      + `See Live <i class="fa fa-clock-o"></i></button>`
+      + `<div class="d-flex pop-buttons">`
+      + `<button class="btn-secondary btn-pop d-flex" type="button">`
+      + `See Source <i class="fa fa-github"></i></button>`
+      + `</div>`
+      + `</div>`
+      + `</div>`
+      + `</div>`
+      + `</div>`
+      + `</div>`
+      + `</div>`;
   
     document.getElementById('overlay').appendChild(divProject);
   
@@ -122,7 +122,7 @@ addEvents();
 seeProject.addEventListener('click', () => {
   modal2.style.display = 'block';
 });
-  
+
 deskCloseBtn.addEventListener('click', () => {
   modal2.style.display = 'none';
 });
@@ -130,7 +130,7 @@ deskCloseBtn.addEventListener('click', () => {
 btnseethis.addEventListener('click', () => {
   modal.style.display = 'block';
 });
-  
+
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
