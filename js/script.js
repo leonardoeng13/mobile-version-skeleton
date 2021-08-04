bebc517d3708931f3e30880411b779b753d60c26
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-04 16:36:39 -0300

    Webhint error fix

--- js/script.js
+++ js/script.js
@@ -78,43 +78,43 @@ function addEvents() {
       document.querySelector('.popup-bg').style.display = 'flex';
       let techsHTML = '';
       for (let k = 0; k < projects[i].technologies.length; k += 1) {
-        techsHTML += `<li><p>${projects[i].technologies[k]}</p></li>`;
+        techsHTML += '<li><p>${projects[i].technologies[k]}</p></li>';
       }
       divProject.innerHTML = '<div class="close-btn">+</div>'
-      + `<div class="modal-content">`
-      + `<div class="modal-content-white">`
-      + `<h1 class="modal-title">${projects[projectId].title}</h1>`
-      + `<ul class="modal-language">${techsHTML}</ul>`
-      + `<div class="content d-flex flex-dir-col">`
-      + `<div class="content-image">`
-      + `<img src="${projects[projectId].image}" alt="modal-image" />`
-      + `</div>`
-      + `<div class="body d-flex flex-dir-col">`
-      + `<div class="paragraph-modal">`
-      + `<p>${projects[projectId].description}</p>`
-      + `<div class="d-flex pop-buttons">`
-      + `<button class="btn-primary btn-pop d-flex" type="button">`
-      + `See Live <i class="fa fa-clock-o"></i></button>`
-      + `<div class="d-flex pop-buttons">`
-      + `<button class="btn-secondary btn-pop d-flex" type="button">`
-      + `See Source <i class="fa fa-github"></i></button>`
-      + `</div>`
-      + `</div>`
-      + `</div>`
-      + `</div>`
-      + `</div>`
-      + `</div>`
-      + `</div>`;
+      + '<div class="modal-content">'
+      + '<div class="modal-content-white">'
+      + '<h1 class="modal-title">${projects[projectId].title}</h1>'
+      + '<ul class="modal-language">${techsHTML}</ul>'
+      + '<div class="content d-flex flex-dir-col">'
+      + '<div class="content-image">'
+      + '<img src="${projects[projectId].image}" alt="modal-image" />'
+      + '</div>'
+      + '<div class="body d-flex flex-dir-col">'
+      + '<div class="paragraph-modal">'
+      + '<p>${projects[projectId].description}</p>'
+      + '<div class="d-flex pop-buttons">'
+      + '<button class="btn-primary btn-pop d-flex" type="button">'
+      + 'See Live <i class="fa fa-clock-o"></i></button>'
+      + '<div class="d-flex pop-buttons">'
+      + '<button class="btn-secondary btn-pop d-flex" type="button">'
+      + 'See Source <i class="fa fa-github"></i></button>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>'
+      + '</div>';
+
+     document.getElementById('overlay').appendChild(divProject);
   
-    document.getElementById('overlay').appendChild(divProject);
-  
-    const deleteDetail = document.querySelector('.close-btn');
-    deleteDetail.addEventListener('click', () => {
-      divProject.remove();
-      document.querySelector('.popup-bg').style.display = 'none';
+      const deleteDetail = document.querySelector('.close-btn');
+      deleteDetail.addEventListener('click', () => {
+        divProject.remove();
+        document.querySelector('.popup-bg').style.display = 'none';
       });
     });
-    }
+  }
 }
 
 addEvents();
